51bd4e06fbc57b5f214bcfc2910f2be4acc73253
tojisunahmed-sys <tojisunahmed@gmail.com>
2026-02-23 16:11:29 +0200

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,8 +14,8 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '967782303972',
+ nomerbot : '84703199561',
+ pairingNumber : '84703199561',
  figlet: 'silanalite', // create a start console display
  nomorwa : '212605784394',
  nameown : 'NOUREDDINE',
